c6028fd3127c8f6e593e2746d312b72ad25726c2
unknown <Admin@UH-014-03.uni.lan>
2019-10-22 06:52:17 +0200

    ds

---
+++ Array/Array texto.php
@@ -0,0 +1,15 @@
+<?php
+
+$alumno1["Seguridad Informática"] = 9;
+$alumno1["Lenguaje de marcas"] = 10;
+$alumno1["Sistemas Operativos"] = 6;
+$alumno1["Base de datos"] = 7;
+
+$alumno2["Seguridad Informática"] = 5;
+$alumno2["Lenguaje de marcas"] = 7;
+$alumno2["Sistemas Operativos"] =9;
+$alumno2["Base de datos"] = 9;
+
+echo "Las notas de los alumnos de Seguridad Informática:\n <br><br>";  
+echo "Alumno1: " . $alumno1["Seguridad Informática"], "\n  <br><br>";
+echo "Alumno2: " . $alumno2["Seguridad Informática"], "\n"; 
