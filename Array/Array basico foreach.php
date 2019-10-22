e52ee9d43315dfc41b37694476fbbc2130a5364c
unknown <Admin@UH-014-03.uni.lan>
2019-10-22 07:17:43 +0200

    vxvxvx

---
+++ Array/Array basico foreach.php
@@ -0,0 +1,18 @@
+<?php 
+
+$v = [1,2,3];
+
+foreach ($v as $x) {
+    echo $x . "<br>";
+    
+}
+
+
+echo "<br>---------------<br>";
+
+$v = ["Jon", "Josu", "Juanjo"];
+
+foreach ($v as $x) {
+    echo $x . "<br>";
+    
+}
