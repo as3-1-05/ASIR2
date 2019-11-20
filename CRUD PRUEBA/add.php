a7a7514a583e035fb031bc2a8e88ba236c1146ce
unknown <Admin@UH-014-03.uni.lan>
2019-11-20 11:44:16 +0100

    ewewew

--- CRUD PRUEBA/add.php
+++ CRUD PRUEBA/add.php
@@ -1,31 +1,54 @@
+<?php session_start(); ?>
+ 
+<?php
+if(!isset($_SESSION['valid'])) {
+    header('Location: login.php');
+}
+?>
+ 
 <html>
 <head>
     <title>Add Data</title>
 </head>
  
 <body>
-    <a href="index.php">Home</a> | <a href="view.php">View Products</a> | <a href="logout.php">Logout</a>
-    <br/><br/>
-    
-    <form action="add.php" method="post" name="form1">
-        <table width="25%" border="0">
-            <tr> 
-                <td>Name</td>
-                <td><input type="text" name="name"></td>
-            </tr>
-            <tr> 
-                <td>Quantity</td>
-                <td><input type="text" name="qty"></td>
-            </tr>
-            <tr> 
-                <td>Price</td>
-                <td><input type="text" name="price"></td>
-            </tr>
-            <tr> 
-                <td></td>
-                <td><input type="submit" name="Submit" value="Add"></td>
-            </tr>
-        </table>
-    </form>
+<?php
+//including the database connection file
+include_once("connection.php");
+ 
+if(isset($_POST['Submit'])) {    
+    $name = $_POST['name'];
+    $qty = $_POST['qty'];
+    $price = $_POST['price'];
+    $loginId = $_SESSION['id'];
+        
+    // checking empty fields
+    if(empty($name) || empty($qty) || empty($price)) {                
+        if(empty($name)) {
+            echo "<font color='red'>Name field is empty.</font><br/>";
+        }
+        
+        if(empty($qty)) {
+            echo "<font color='red'>Quantity field is empty.</font><br/>";
+        }
+        
+        if(empty($price)) {
+            echo "<font color='red'>Price field is empty.</font><br/>";
+        }
+        
+        //link to the previous page
+        echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+    } else { 
+        // if all the fields are filled (not empty) 
+            
+        //insert data to database    
+        $result = mysqli_query($mysqli, "INSERT INTO products(name, qty, price, login_id) VALUES('$name','$qty','$price', '$loginId')");
+        
+        //display success message
+        echo "<font color='green'>Data added successfully.";
+        echo "<br/><a href='view.php'>View Result</a>";
+    }
+}
+?>
 </body>
 </html>
