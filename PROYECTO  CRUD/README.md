832ca5e73c1e5758b5a1d3a9ba5078a79ad2c5d7
unknown <Admin@UH-014-03.uni.lan>
2019-11-20 11:30:17 +0100

    wwqwq

--- PROYECTO  CRUD/README.md
+++
@@ -1,3 +0,0 @@
-# CRUD PHP MySQL
-
-https://www.w3schools.com/php/php_mysql_intro.asp
--- PROYECTO  CRUD/read.php
+++ PROYECTO  CRUD/read.php
@@ -1,29 +1,91 @@
 <?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "joncrud";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-//
-$sql = "SELECT * FROM empleados";
-
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. "<br> nombre: " . $row["nombre"]. "<br> apellido:" . $row["apellido"]. "<br> <img width='10%' src=img/" . $row["img"] . "> <br><hr/>";
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM employees WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $salary = $row["salary"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
     }
-} else {
-    echo "0 results";
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
 }
-
-$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View Record</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <p class="form-control-static"><?php echo $row["name"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Address</label>
+                        <p class="form-control-static"><?php echo $row["address"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Salary</label>
+                        <p class="form-control-static"><?php echo $row["salary"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
