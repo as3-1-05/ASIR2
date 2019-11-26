381da3cb3f5ef29161cd1ce9cb51fd4a3d3a70d3
unknown <Admin@UH-014-03.uni.lan>
2019-11-26 08:16:04 +0100

    ss

--- CRUD/index.php
+++ CRUD/index.php
@@ -36,22 +36,22 @@ if($link === false){
 }
  
 // Attempt select query execution
-$sql = "SELECT * FROM persons";
+$sql = "SELECT * FROM products";
 if($result = mysqli_query($link, $sql)){
     if(mysqli_num_rows($result) > 0){
         echo "<table>";
             echo "<tr>";
                 echo "<th>id</th>";
-                echo "<th>first_name</th>";
-                echo "<th>last_name</th>";
-                echo "<th>email</th>";
+                echo "<th>name</th>";
+                echo "<th>qty</th>";
+                echo "<th>price</th>";
             echo "</tr>";
         while($row = mysqli_fetch_array($result)){
             echo "<tr>";
                 echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['first_name'] . "</td>";
-                echo "<td>" . $row['last_name'] . "</td>";
-                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['qty'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
