d4ebf330e1ec7d741b7cf858bff63355d6e18369
unknown <Admin@UH-014-03.uni.lan>
2019-11-26 07:44:18 +0100

    fff

--- CRUD/delete.php
+++ CRUD/delete.php
@@ -7,15 +7,15 @@ if(!isset($_SESSION['valid'])) {
 ?>
  
 <?php
-//including the database connection file
+
 include("connection.php");
  
-//getting id of the data from url
+
 $id = $_GET['id'];
  
-//deleting the row from table
+
 $result=mysqli_query($mysqli, "DELETE FROM products WHERE id=$id");
  
-//redirecting to the display page (view.php in our case)
+
 header("Location:view.php");
 ?>
