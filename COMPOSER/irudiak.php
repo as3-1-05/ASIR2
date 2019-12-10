61c2039c8deb5864c120238fc6e7fe9b2badda8b
unknown <Admin@UH-014-03.uni.lan>
2019-12-10 08:27:23 +0100

    s

---
+++ COMPOSER/irudiak.php
@@ -0,0 +1,12 @@
+<?php 
+
+// include composer autoload
+require 'vendor/autoload.php';
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManager;
+
+// create an image manager instance with favored driver
+$manager = new ImageManager(array('driver' => 'imagick'));
+
+// to finally create image instances
+$image = $manager->make('public/foo.jpg')->resize(300, 200);
