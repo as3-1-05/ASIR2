e06ead383eb2cc73c80334506bf5ee8abf466352
admin <as3-2-06.19@uni.eus>
2020-01-14 08:20:24 +0000

    SS

--- COMPOSER/weather-forecast/weather-forecast.php
+++ COMPOSER/weather-forecast/weather-forecast.php
@@ -2,8 +2,9 @@
 
 require 'vendor/autoload.php';
 
-use DarkSkyApi;
-$forecast = DarkSkyApi::location(46.482, 30.723)
+use DmitryIvanov\DarkSkyApi\DarkSkyApi;
+$forecast = (new DarkSkyApi('e41d8193b1312dc0eb8f4916dcfe5e1a'))
+    ->location(46.482, 30.723)
     ->units('si')
     ->language('es')
     ->forecast('currently');
