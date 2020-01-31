98fb62c8f697deb7dbd50a3c2f47589ee915e2f5
as3-1-05 <as3-1-05@uni.eus>
2020-01-31 08:49:49 +0000

    ss

--- WEATHER-FORECAST/index.php
+++ WEATHER-FORECAST/index.php
@@ -30,6 +30,7 @@
                 <div class="card p-4" style="margin: 0 auto; max-width: 320px;">
                     <h2>Eguraldia</h2>
                     <h3 class="display-2"><?php echo $temperature_current; ?>&deg;</h3>
+                    <p class="lead"><?php echo $summary_current; ?></p>
                 </div>
         </main>
     </body>
