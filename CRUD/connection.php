fe446469440dda4724fa454eff950492ca7c2352
as3-1-05 <as3-1-05@uni.eus>
2020-02-03 07:51:23 +0000

    ss

--- CRUD/connection.php
+++ CRUD/connection.php
@@ -3,7 +3,7 @@
  
 $databaseHost = 'localhost';
 $databaseName = 'test2';
-$databaseUsername = 'admin';
+$databaseUsername = 'root';
 $databasePassword = '';
  
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
