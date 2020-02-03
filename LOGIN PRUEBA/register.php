6d334fad7015d774e6731eccb326415020361353
as3-1-05 <as3-1-05@uni.eus>
2020-02-03 10:40:48 +0000

    ss

--- LOGIN PRUEBA/register.php
+++ LOGIN PRUEBA/register.php
@@ -1,19 +1,19 @@
 <html>
 <head>
-    <title>Register</title>
+    <title>Erregistratu</title>
 </head>
-
+ 
 <body>
-    <a href="index.php">Home</a> <br />
+    <a href="index.php">Itzuli</a> <br />
     <?php
     include("connection.php");
-
+ 
     if(isset($_POST['submit'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $user = $_POST['username'];
         $pass = $_POST['password'];
-
+ 
         if($user == "" || $pass == "" || $name == "" || $email == "") {
             echo "All fields should be filled. Either one or many fields are empty.";
             echo "<br/>";
@@ -21,40 +21,37 @@
         } else {
             mysqli_query($mysqli, "INSERT INTO login(name, email, username, password) VALUES('$name', '$email', '$user', md5('$pass'))")
             or die("Could not execute the insert query.");
-			
+            
             echo "Registration successfully";
             echo "<br/>";
             echo "<a href='login.php'>Login</a>";
         }
     } else {
 ?>
-        <p><font size="+2">Register</font></p>
+        <p><font size="+2">Erregistratu</font></p>
         <form name="form1" method="post" action="">
             <table width="75%" border="0">
                 <tr> 
-                    <td width="10%">Full Name</td>
+                    <td width="10%">Izen / Abizena</td>
                     <td><input type="text" name="name"></td>
                 </tr>
                 <tr> 
                     <td>Email</td>
                     <td><input type="text" name="email"></td>
-                </tr>			
+                </tr>            
                 <tr> 
-                    <td>Username</td>
+                    <td>Erabiltzailea</td>
                     <td><input type="text" name="username"></td>
                 </tr>
                 <tr> 
-                    <td>Password</td>
+                    <td>Pasahitza</td>
                     <td><input type="password" name="password"></td>
                 </tr>
                 <tr> 
                     <td>&nbsp;</td>
-                    <td><input type="submit" name="submit" value="Submit"></td>
+                    <td><input type="submit" name="submit" value="Sortu"></td>
                 </tr>
             </table>
         </form>
     <?php
     }
-    ?>
-</body>
-</html>
