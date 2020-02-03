d828f944bc7862b0baad8013dae8a59fdfa00295
as3-1-05 <as3-1-05@uni.eus>
2020-02-03 09:10:32 +0000

    cc

--- CRUD/index.php
+++ CRUD/index.php
@@ -34,36 +34,6 @@ $link = mysqli_connect("localhost", "root", "", "test2");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
-// Attempt select query execution
-$sql = "SELECT * FROM products";
-if($result = mysqli_query($link, $sql)){
-    if(mysqli_num_rows($result) > 0){
-        echo "<table width='80%' border=0>";
-            echo "<tr bgcolor='#CCCCCC'>";
-                echo "<td>id</td>";
-                echo "<td>nombre</td>";
-                echo "<td>cantidad</td>";
-                echo "<td>precio</td>";
-            echo "</tr>";
-        while($row = mysqli_fetch_array($result)){
-            echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['name'] . "</td>";
-                echo "<td>" . $row['qty'] . "</td>";
-                echo "<td>" . $row['price'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        // Free result set
-        mysqli_free_result($result);
-    } else{
-        echo "No records matching your query were found.";
-    }
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
- 
 // Close connection
 mysqli_close($link);
 ?>
